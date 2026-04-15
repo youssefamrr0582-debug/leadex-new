50381db3bbeb4ec2a0c43322901c34978d65a1bb
youssefamrr0582-debug <youssefamrr0582@gmail.com>
2026-04-15 19:56:05 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#070b14",
+      color: "white",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "sans-serif"
+    }}>
+      <h1 style={{ color: "#34d399", fontSize: "50px" }}>
+        Leadex
+      </h1>
+      <p>B2B Appointment Setting Agency</p>
+    </div>
+  )
+}
