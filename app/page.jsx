5aaf5917719e379d3cefa5d44896def36a396180
youssefamrr0582-debug <youssefamrr0582@gmail.com>
2026-04-20 21:41:45 +0200

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,28 @@
+export default function Page() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#f7f4ef",
+      padding: 50,
+      fontFamily: "Arial"
+    }}>
+      
+      <h1 style={{
+        fontSize: 50,
+        color: "#ff7a00",
+        fontWeight: "bold"
+      }}>
+        LeadEX 🚀
+      </h1>
+
+      <p style={{ fontSize: 18, marginTop: 10 }}>
+        B2B Lead Generation & Appointment Setting in Australia
+      </p>
+
+      <p style={{ marginTop: 25, fontWeight: "bold", fontSize: 20 }}>
+        Stop Chasing Leads. Start Closing Deals.
+      </p>
+
+    </div>
+  );
+}
